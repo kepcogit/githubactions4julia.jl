63991eeaa80a7b144063b6cad05dd733ba0ac36d
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-04-10 13:56:15 +0000

    build based on 3c15d64

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#","page":"Home","title":"Home","text":"CurrentModule = githubactions4julia","category":"page"},{"location":"#githubactions4julia-1","page":"Home","title":"githubactions4julia","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [githubactions4julia]","category":"page"}]
+}
